b409258cec305d986e7f80216810f9381e87ab92
Anna Shipman <anna@annashipman.co.uk>
2013-10-27 11:09:44 +0000

    A start at a CSS animation

---
+++ animation.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+$("#scene-01").append(
+    '<div id="bg-01">'
+    + '<div id="animation-demo" class="hover">'
+    + '<div id="stalk"><img src="stalk.png" alt=""></div>' 
+    + '<div id="petal-01"><img src="petal-01.png" alt=""></div>'
+    + '<div id="petal-02"><img src="petal-02.png" alt=""></div>'
+    + '<div id="petal-03"><img src="petal-03.png" alt=""></div>' 
+    + '<div id="petal-04"><img src="petal-04.png" alt=""></div>' 
+    + '<div id="petal-05"><img src="petal-05.png" alt=""></div>'
+    + '<div id="petal-06"><img src="petal-06.png" alt=""></div>' 
+    + '<div id="petal-07"><img src="petal-07.png" alt=""></div>' 
+    + '<div id="petal-08"><img src="petal-08.png" alt=""></div>' 
+    + '<div id="petal-09"><img src="petal-09.png" alt=""></div>'
+    + '</div>'
+    + '</div>');
+});
+
